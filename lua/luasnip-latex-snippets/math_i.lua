941066844f021e9c404654d4c7a87c23f8eba7db
Dennis Klein <sycx22@gmail.com>
2023-04-14 13:21:20 +0200

    - fix sum to use limits

--- lua/luasnip-latex-snippets/math_i.lua
+++ lua/luasnip-latex-snippets/math_i.lua
@@ -3,12 +3,12 @@ local ls = require("luasnip")
 local math_i = {
   ls.parser.parse_snippet(
     { trig = "sum", name = "sum" },
-    "\\sum_{n=${1:1}}^{${2:\\infty}} ${3:a_n z^n}"
+    "\\sumlimits_{n=${1:1}}^{${2:\\infty}} ${3:a_n z^n}"
   ),
 
   ls.parser.parse_snippet(
     { trig = "taylor", name = "taylor" },
-    "\\sum_{${1:k}=${2:0}}^{${3:\\infty}} ${4:c_$1} (x-a)^$1 $0"
+    "\\sumlimits_{${1:k}=${2:0}}^{${3:\\infty}} ${4:c_$1} (x-a)^$1 $0"
   ),
 
   ls.parser.parse_snippet({ trig = "lim", name = "limit" }, "\\lim_{${1:n} \\to ${2:\\infty}} "),
